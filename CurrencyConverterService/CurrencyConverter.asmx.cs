262cd44db2f803fedcffa4bba1f4b65290e3caf9
Hasan <hasansiddiqui17098@gmail.com>
2025-05-20 10:26:07 +0500

    Add project files.

---
+++ CurrencyConverterService/CurrencyConverter.asmx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Services;
+
+namespace CurrencyConverterService
+{
+    /// <summary>
+    /// Summary description for CurrencyConverter
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+
+    public class CurrencyConverter : System.Web.Services.WebService
+    {
+        private static readonly List<string> MaliciousPatterns = new List<string>
+        {
+            @"SELECT\s+\*",  // SQL SELECT *
+            @"DROP\s+TABLE", // SQL DROP TABLE
+            @"<script>",     // XSS script tag
+            @"rm\s+-rf"      // Shell command to remove files recursively
+        };
+
+        [WebMethod]
+        public string CheckForMaliciousPatterns(string inputScript)
+        {
+            if (string.IsNullOrWhiteSpace(inputScript))
+                return "Error: Input script cannot be empty.";
+
+            foreach (var pattern in MaliciousPatterns)
+            {
+                if (Regex.IsMatch(inputScript, pattern, RegexOptions.IgnoreCase))
+                {
+                    return "Malicious pattern detected.";
+                }
+            }
+
+            return "The script is safe.";
+        }
+    }
+}
---
+++ CurrencyConverterService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CurrencyConverterService.MvcApplication" Language="C#" %>
